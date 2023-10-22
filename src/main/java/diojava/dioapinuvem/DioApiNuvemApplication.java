9463413f430ece92ed5e7ed96e0b522964aaaf5f
Incryve <67076293+Incryve@users.noreply.github.com>
2023-10-21 22:25:18 -0300

    Initial commit

---
+++ src/main/java/diojava/dioapinuvem/DioApiNuvemApplication.java
@@ -0,0 +1,13 @@
+package diojava.dioapinuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioApiNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioApiNuvemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/diojava/dioapinuvem/DioApiNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package diojava.dioapinuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioApiNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
